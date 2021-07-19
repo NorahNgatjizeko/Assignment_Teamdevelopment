b0c9e2e2b7c53b27f1fd96d9b0989f548b615463
norah ngatjizeko <tjeripo86@live.com>
2021-07-19 18:45:26 +0900

    issue2

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -17,7 +17,7 @@ class TeamsController < ApplicationController
 
   def edit
   end
-  
+
   def create
     @team = Team.new(team_params)
     @team.owner = current_user
@@ -42,6 +42,15 @@ class TeamsController < ApplicationController
   def destroy
     @team.destroy
     redirect_to teams_url, notice: I18n.t('views.messages.delete_team')
+  end
+
+  def owner
+    @team = Team.friendly.find(params[:team_id])
+    new_owner = Assign.find(params[:id])
+    @team.owner = new_owner.user
+    @team.update(team_params)
+    OwnerMailer.owner_mail(new_owner.user.email, @team.name).deliver
+    redirect_to team_url(params[:team_id])
   end
 
   def dashboard
